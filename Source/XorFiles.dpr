74602c0c7deb545d7c725139b112ca14776e1af8
b.bocharov <b.bocharov@argustelecom.ru>
2019-11-05 18:09:11 +0300

    Initial implementation

---
+++ Source/XorFiles.dpr
@@ -0,0 +1,134 @@
+﻿program XorFiles;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  Windows,
+  Classes,
+  SysUtils,
+  StrUtils,
+  XorUtils in 'XorUtils.pas';
+
+function ChangeFileExt(const FileName : String; Xored : Boolean) : String;
+const
+  ARR_EXTENSIONS : array [Boolean] of String = ('.unxored', '.xored');
+begin
+  if EndsText(ARR_EXTENSIONS[Xored], FileName) then
+    Result := FileName
+  else
+  if EndsText(ARR_EXTENSIONS[not Xored], FileName) then
+    Result := SysUtils.ChangeFileExt(FileName, ARR_EXTENSIONS[Xored])
+  else
+    Result := FileName + ARR_EXTENSIONS[Xored];
+end;
+
+procedure ExcludePreviouslyProducedFiles(FileNames : TStringList);
+var
+  L : TStringList;
+  i : Integer;
+  sFileName, sXoredFileName, sUnXoredFileName : String;
+begin
+  L := TStringList.Create;
+  try
+    L.Assign(FileNames);
+
+    i := 0;
+    while i < L.Count do
+    begin
+      sFileName        := L[i];
+      sXoredFileName   := ChangeFileExt(sFileName, True);
+      sUnXoredFileName := ChangeFileExt(sFileName, False);
+
+      if SameText(sFileName, sXoredFileName) or SameText(sFileName, sUnXoredFileName) then
+        L.Delete(i)
+      else
+        Inc(i);
+    end;
+
+    FileNames.Assign(L);
+  finally
+    L.Free;
+  end;
+end;
+
+procedure ProcessFile(const FileName : String; const Key : String);
+var
+  InFS, XoredFS, UnXoredFS : TFileStream;
+begin
+  InFS := TFileStream.Create(FileName, fmOpenRead + fmShareDenyWrite);
+  try
+    XoredFS := TFileStream.Create(ChangeFileExt(FileName, True), fmCreate);
+    try
+      XorData(InFS, Key, XoredFS);
+
+      UnXoredFS := TFileStream.Create(ChangeFileExt(FileName, False), fmCreate);
+      try
+        XoredFS.Position := 0;
+        XorData(XoredFS, Key, UnXoredFS);
+      finally
+        UnXoredFS.Free;
+      end;
+    finally
+      XoredFS.Free;
+    end;
+  finally
+    InFS.Free;
+  end;
+end;
+
+const
+  faNormal = {$IF CompilerVersion >= 20.0}SysUtils.faNormal{$ELSE}$00000080{$IFEND};
+var
+  sDir, sKey : String;
+  SR : TSearchRec;
+  FileNames : TStringList;
+  sFileName : String;
+  iBenchmark : Cardinal;
+begin
+  try
+    sDir := IncludeTrailingPathDelimiter(ExtractFileDir(ParamStr(0)));
+    Writeln('Working directory: ', sDir);
+
+    Writeln('Enter key:');
+    Readln(sKey);
+    Writeln('Key: ', sKey);
+
+    FileNames := TStringList.Create;
+    try
+      if FindFirst(sDir + '*', faNormal, SR) = S_OK then
+        try
+          repeat
+            FileNames.Add(sDir + SR.Name);
+          until FindNext(SR) <> S_OK;
+        finally
+          FindClose(SR);
+        end;
+      ExcludePreviouslyProducedFiles(FileNames);
+      Writeln('Found files: ', FileNames.Count);
+
+      for sFileName in FileNames do
+      begin
+        Writeln('Processing: ', ExtractFileName(sFileName));
+
+        iBenchmark := GetTickCount;
+        ProcessFile(sFileName, sKey);
+        iBenchmark := GetTickCount - iBenchmark;
+
+        Writeln('Processed: ', iBenchmark, ' ms (~', iBenchmark div 2, ' ms per operation)');
+      end;
+    finally
+      FileNames.Free;
+    end;
+
+    Writeln('Press any key to exit...');
+    Readln;
+  except
+    on E: Exception do
+    begin
+      Writeln(E.ClassName, ': ', E.Message);
+      Readln;
+    end;
+  end;
+end.
---
+++ Source/XorUtils.pas
@@ -0,0 +1,141 @@
+﻿unit XorUtils;
+
+interface
+
+uses
+  Classes;
+
+  procedure XorData(Input : TStream; const Key : String; Output : TStream); overload;
+  procedure XorData(Input : TStream; BufferSize : Cardinal; const Key : String; Output : TStream); overload;
+  procedure XorData(Input : TStream; Count : Int64; BufferSize : Cardinal;
+    const Key : String; Output : TStream); overload;
+
+implementation
+
+uses
+  SysUtils, Math;
+
+type
+
+  TXorableOrdinal = Int64;
+
+  TBufferElement = TXorableOrdinal;
+  TBuffer = array of TBufferElement;
+
+  TKeyElement = TXorableOrdinal;
+  TKey = array of TKeyElement;
+
+const
+
+  BUFFER_ELEMENT_SIZE = SizeOf(TBufferElement);
+  KEY_ELEMENT_SIZE = SizeOf(TKeyElement);
+
+  DEFAULT_BUFFER_SIZE : Cardinal = 16 * 1024 * 1024;  // 16 MB
+  DEFAULT_COUNT : Int64 = -1; // ALL
+
+{ INTERNAL }
+
+procedure CheckParams(Input : TStream; Count : Int64; BufferSize : Cardinal;
+  const Key : String; Output : TStream);
+begin
+  Assert(Input <> nil, 'Input = nil');
+  Assert(Count >= -1, 'Count < -1');
+  Assert(BufferSize mod BUFFER_ELEMENT_SIZE = 0, 'Invalid BufferSize');
+  Assert(Length(Key) > 0, 'Key = <empty>');
+  Assert(Output <> nil, 'Output = nil');
+  Assert(Input <> Output, 'Input = Output');
+end;
+
+function CreateBuffer(BufferSize : Cardinal) : TBuffer;
+begin
+  SetLength(Result, BufferSize div BUFFER_ELEMENT_SIZE);
+end;
+
+function CreateKey(const Key : String) : TKey;
+var
+  iKeySize : Integer;
+begin
+  iKeySize := Length(Key) * SizeOf(Char);
+  SetLength(Result, iKeySize div KEY_ELEMENT_SIZE + 1);
+  Move(Key[1], Result[0], iKeySize);
+end;
+
+function GetActualCount(Input : TStream; Count : Int64) : Int64;
+begin
+  Result := Input.Size - Input.Position;
+  if Count <> DEFAULT_COUNT then
+    Result := Min(Result, Count);
+end;
+
+function XorElement(Element : TBufferElement; const Key : TKey) : TBufferElement; inline;
+var
+  i : Integer;
+begin
+  Result := Element;
+  for i := 0 to High(Key) do
+    Result := Result xor Key[i];
+end;
+
+procedure XorBuffer(const Input : TBuffer; Count : Integer; const Key : TKey; const Output : TBytes); inline;
+var
+  iOutputOffset, i : Integer;
+  XoredElement : TBufferElement;
+begin
+  iOutputOffset := 0;
+
+  for i := 0 to Count div BUFFER_ELEMENT_SIZE do
+  begin
+    XoredElement := XorElement(Input[i], Key);
+    Move(XoredElement, Output[iOutputOffset], BUFFER_ELEMENT_SIZE);
+    Inc(iOutputOffset, BUFFER_ELEMENT_SIZE);
+  end;
+
+  if Count mod BUFFER_ELEMENT_SIZE > 0 then
+    // TODO: handle insufficient or remaining bytes
+end;
+
+procedure XorData(Input : TStream; Count : Int64; const Buffer : TBuffer;
+  const Key : TKey; Output : TStream); overload;
+var
+  iBufferSize, iChunkSize : Integer;
+  XoredChunk : TBytes;
+begin
+  if Count = 0 then
+    Exit;
+
+  iBufferSize := Length(Buffer) * BUFFER_ELEMENT_SIZE;
+  repeat
+    FillChar(Buffer[0], iBufferSize, 0);
+    iChunkSize := Input.Read(Buffer[0], iBufferSize);
+    if iChunkSize <= 0 then
+      Break;
+
+    SetLength(XoredChunk, iChunkSize);
+    FillChar(XoredChunk[0], iChunkSize, 0);
+    XorBuffer(Buffer, iChunkSize, Key, XoredChunk);
+    Output.WriteBuffer(XoredChunk[0], iChunkSize);
+
+    Dec(Count, iChunkSize);
+  until Count <= 0;
+end;
+
+{ PUBLIC }
+
+procedure XorData(Input : TStream; const Key : String; Output : TStream);
+begin
+  XorData(Input, DEFAULT_BUFFER_SIZE, Key, Output);
+end;
+
+procedure XorData(Input : TStream; BufferSize : Cardinal; const Key : String; Output : TStream);
+begin
+  XorData(Input, DEFAULT_COUNT, BufferSize, Key, Output);
+end;
+
+procedure XorData(Input : TStream; Count : Int64; BufferSize : Cardinal;
+  const Key : String; Output : TStream);
+begin
+  CheckParams(Input, Count, BufferSize, Key, Output);
+  XorData(Input, GetActualCount(Input, Count), CreateBuffer(BufferSize), CreateKey(Key), Output);
+end;
+
+end.
